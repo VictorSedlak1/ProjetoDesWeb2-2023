2f287ab2e58ed8c61d938379f38dda2abd19b458
VictorSedlak1 <victorsedlak04@gmail.com>
2023-03-10 16:49:17 -0300

    ObaBear_03

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,17 @@ function zerarContador(){
 onMounted(() => {
   console.log(`O valor inicial do contador é ${contador.value}.`)
 })
+
+const nome = 'João'
+    const idade = 25
+
+    function inverter(texto) {
+        return texto.split('').reverse().join('')
+    }
+
+    function saudacao() {
+        return `Olá, ${nome}!`
+    }
 </script>
 
 <template>
@@ -28,10 +39,36 @@ onMounted(() => {
   <button @click="zerarContador">zerar</button>
 
   <p>Valor do contador é: {{ contador }}</p>
+
+  <div class="info">
+        <h1>Exemplo 1 - Mostrando informações na tela</h1>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }}  anos</p>
+        <hr />
+        <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+        <p>O nome tem {{ nome.length }}  caracteres</p>
+        <p>O nome invertido é: {{ inverter(nome) }} </p>
+        <hr />
+        <p> Exemplo de saudação usando função</p>
+        <p> {{ saudacao() }}  </p>
+    </div>
 </template>
 
 <style scoped>
 button {
   font-weight: bold;
+}
+
+.info {
+  background-color: rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+}
+
+h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 1.5em;
 }
 </style>
